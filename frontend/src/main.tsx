b06ffe467d78d7249d30d040d34fc989698f1a54
RamiBouGhanem <rami.boughanem240@gmail.com>
2025-11-10 14:38:18 +0200

    solving refreshing issue

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+// Handle client-side routing redirects
+const redirect = sessionStorage.getItem('redirect');
+if (redirect) {
+  sessionStorage.removeItem('redirect');
+  window.history.replaceState(null, '', redirect);
+}
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
